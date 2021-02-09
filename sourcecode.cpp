7e38e5ed476c2dea1ed0fdfe0a589ec391183735
N E W E <59024021+NEWE69@users.noreply.github.com>
2021-02-09 17:58:58 +0100

    Update sourcecode.cpp

--- sourcecode.cpp
+++ sourcecode.cpp
@@ -1,10 +1,15 @@
 /// Author : N E W E
 /// Date : 26/01/2021
-/// File : discord nitro generator
+
 
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <chrono>
+#include <thread>
+#include <fstream>
+
+
 
 int main()
 {
@@ -14,9 +19,21 @@ int main()
 	srand(time(0));
 
 	std::string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	
 
+	std::cout << R"(
+	  ___  _                   _    ___ _  __ _      ___                       _           
+	 |   \(_)___ __ ___ _ _ __| |  / __(_)/ _| |_   / __|___ _ _  ___ _ _ __ _| |_ ___ _ _ 
+	 | |) | (_-</ _/ _ \ '_/ _` | | (_ | |  _|  _| | (_ / -_) ' \/ -_) '_/ _` |  _/ _ \ '_|
+	 |___/|_/__/\__\___/_| \__,_|  \___|_|_|  \__|  \___\___|_||_\___|_| \__,_|\__\___/_|  
+	 )" << std::endl;
+	
 	std::cout << "How many discord link ?\n" << std::endl;
 	std::cin >> number;
+
+	//create or open the .txt document
+	std::ofstream myfile("discord.txt");
+
 
 	if (number < 50000) {
 
@@ -28,10 +45,9 @@ int main()
 			while (line != 15)
 			{
 				//random
-
 				int random = rand() % letters.size();
 
-				//assigner une lettre a chaque chiffre
+				//numbers = letters
 				link += letters[random];
 
 				line += 1;
@@ -39,7 +55,13 @@ int main()
 
 			std::cout << "https://discord.gift/" << link << std::endl;
 
-
+			//check the .txt document + write the gift codes in
+			if (myfile){
+				myfile << "https://discord.gift/" << link << "\n";
+			}
+			else {
+				std::cout <<"ERROR"<< std::endl;
+			}
 
 			count += 1;
 		}
@@ -50,7 +72,10 @@ int main()
 	}
 
 	std::cout << "DONE \n" << std::endl;
+	std::cout << "Discord : NEWE#9471 \nTelegram : @NEWE69 \n" << std::endl;
 
+	myfile << "Discord : NEWE#9471 \nTelegram : @NEWE69 \n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	std::cin.ignore();
 	return 0;
 }
